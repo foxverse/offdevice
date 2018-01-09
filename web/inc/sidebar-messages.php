74cf97838eb21a9ee9a12aebec03fac1b9148c5d
xKyu <antherynx.studio@mail.com>
2018-01-08 23:11:59 -0500

    ok

--- web/inc/sidebar-messages.php
+++
@@ -1,8 +0,0 @@
-<div class="post-list-outline" style="text-align: center">
-	<h2 class="label">Say bye to Closedverse, Openverse and Cedar. Let's simplify this.</h2>
-	<p style="width: 90%; display: inine-block; padding: 10px;"><iframe src="https://www.youtube.com/embed/Mi8za2kTJDs?autoplay=1" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></p>
-</div>
-<div class="post-list-outline" style="text-align: center">
-	<h2 class="label">Why?</h2>
-	<p style="width: 90%; display: inine-block; padding: 10px;">Because they all fucking suck, lets admit it.</p>
-</div>
